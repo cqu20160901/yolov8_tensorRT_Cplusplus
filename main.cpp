f1a9705da50d9009a423dd79e9f2a7dc0f8ac4d4
张潜 <1013429609@qq.com>
2024-10-10 20:21:59 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,15 +3,14 @@
 
 int main()
 {
-    std::string OnnxFile = "/zhangqian/workspaces1/TensorRT/yolov8_trt_Cplusplus/models/yolov8n_ZQ.onnx";
-    std::string SaveTrtFilePath = "/zhangqian/workspaces1/TensorRT/yolov8_trt_Cplusplus/models/yolov8n_ZQ.trt";
-    cv::Mat SrcImage = cv::imread("/zhangqian/workspaces1/TensorRT/yolov8_trt_Cplusplus/images/test.jpg");
+    std::string OnnxFile = "/home/zq/Detect/yolov8_tensorRT_Cplusplus-master/models/yolov8n_ZQ.onnx";
+    std::string SaveTrtFilePath = "/home/zq/Detect/yolov8_tensorRT_Cplusplus-master/models/yolov8n_ZQ.trt";
+    cv::Mat SrcImage = cv::imread("/home/zq/Detect/yolov8_tensorRT_Cplusplus-master/images/test.jpg");
 
     int img_width = SrcImage.cols;
     int img_height = SrcImage.rows;
 
-    CNN YOLO(OnnxFile, SaveTrtFilePath, 1, 3, 640, 640, 7);
-    YOLO.ModelInit();
+    CNN YOLO(OnnxFile, SaveTrtFilePath, 1, 3, 640, 640);
     YOLO.Inference(SrcImage);
 
     for (int i = 0; i < YOLO.DetectiontRects_.size(); i += 6)
@@ -29,7 +28,7 @@ int main()
         putText(SrcImage, text1, cv::Point(xmin, ymin + 15), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(0, 0, 255), 2);
     }
 
-    imwrite("/zhangqian/workspaces1/TensorRT/yolov8_trt_Cplusplus/images/result.jpg", SrcImage);
+    imwrite("/home/zq/Detect/yolov8_tensorRT_Cplusplus-master/images/result.jpg", SrcImage);
 
     printf("== obj: %d \n", int(float(YOLO.DetectiontRects_.size()) / 6.0));
 
