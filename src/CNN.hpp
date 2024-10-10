23816f3599292a078eaeed0f9a459ae79d46b95f
张潜 <1013429609@qq.com>
2024-10-10 20:06:58 +0800

    Update CNN.hpp

--- src/CNN.hpp
+++ src/CNN.hpp
@@ -5,19 +5,26 @@
 #include "postprocess.hpp"
 #include <opencv2/opencv.hpp>
 
+#include <cuda_runtime.h>
+#include <npp.h>
+#include <nppi.h>
+#include <nppdefs.h>
+
+
 class CNN
 {
 public:
-    CNN(const std::string &OnnxFilePath, const std::string &SaveTrtFilePath, int BatchSize, int InputChannel, int InputImageWidth, int InputImageHeight, int ModelOutHeadNum);
+    CNN(const std::string &OnnxFilePath, const std::string &SaveTrtFilePath, int BatchSize, int InputChannel, int InputImageWidth, int InputImageHeight);
     ~CNN();
-    void ModelInit();
+
     void Inference(cv::Mat &SrcImage);
 
     std::vector<float> DetectiontRects_;
 
 private:
-    void EngineInference(cv::Mat &SrcImage, void **Buffers, const std::vector<int64_t> &BufferSize, cudaStream_t Stream);
-    std::vector<float> PrepareImage(cv::Mat &vec_img);
+    void ModelInit();
+    void PrepareImage(cv::Mat &vec_img, std::vector<float> &PreprocessResult);
+    void PrepareImage(cv::Mat &vec_img, void *InputBuffer);
 
     std::string OnnxFilePath_;
     std::string SaveTrtFilePath_;
@@ -26,11 +33,25 @@ private:
     int InputChannel_ = 0;
     int InputImageWidth_ = 0;
     int InputImageHeight_ = 0;
-    int ModelOutHeadNum_ = 0;
     int ModelOutputSize_ = 0;
 
     nvinfer1::ICudaEngine *PtrEngine_ = nullptr;
     nvinfer1::IExecutionContext *PtrContext_ = nullptr;
+    cudaStream_t Stream_;
+
+    void *Buffers_[10];
+    std::vector<int64_t> BuffersDataSize_;
+    std::vector<float *> OutputData_;
+    std::vector<float> PreprocessResult_;
+
+    Npp8u *GpuSrcImgBuf_ = nullptr;     // gpu：装 src 图像
+    Npp8u *GpuImgResizeBuf_ = nullptr;  //  gpu 装 resize后的图像
+    Npp32f *GpuImgF32Buf_ = nullptr;    // gpu: int8 转 F32
+    Npp32f *GpuDataPlanes_ = nullptr;
+
+    Npp32f MeanScale_[3] = {0.00392157, 0.00392157, 0.00392157};
+    int DstOrder_[3] = {2, 1, 0};
+    Npp32f* DstPlanes_[3];
 };
 
-#endif // YOLOV6_TRT_YOLOV6_H
+#endif
